065c8aa439f8fd3eae5917dfc591bb7046127982
cotteputman <cotteputman@outlook.com>
2024-10-08 16:51:13 +0800

    TPG与DG脱轨

--- tpg/TPG.py
+++ tpg/TPG.py
@@ -30,8 +30,8 @@ class TemporalPortfolioGraph:
     
     每一个TPG应当唯一对应一个DataGenerator
     """
+    # 初始化TPG不需要提供信息
     def __init__(self,
-                 data_gen: DataGenerator,
                  input_dim=12,
                  output_dim=128, 
                  hidden_dim=128,
@@ -44,27 +44,17 @@ class TemporalPortfolioGraph:
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.hidden_dim = hidden_dim
-        self.src = data_gen
         self.device = device
 
+    # TODO 或许可以考虑更多可变参数
+    def reset(self, 
+              num_asset: int, 
+              price_feature: np.array):
+        """
+        重置图相关数据，根据PM_Env的初始化来进行
+        """
+        self.num_asset = num_asset
         # 初始化边邻接矩阵
-        self.weighted_adj = None
-
-        self.is_reset = False
-    
-    def reset(self):
-        """
-        重置图相关数据
-        """
-        # 利用src进行初始化的变量需要严格等待DataGenerator.reset()后再进行
-        if not self.src.is_reset:
-            try:
-                self.src.reset()
-                raise RuntimeWarning("The reset of DataGenerator should be conducted outside TPG.")
-            except Exception as e:
-                raise RuntimeError(f"{e}: TPG can only be reset after DataGenerator is reset!")
-        
-        self.num_asset = self.src.data.shape[0]
         self.weighted_adj = torch.zeros((self.num_asset, self.num_asset), dtype=torch.float).to(self.device)
 
         # TODO 处理输入维度算法
@@ -72,10 +62,10 @@ class TemporalPortfolioGraph:
                                     self.input_dim, 
                                     self.hidden_dim, 
                                     self.output_dim)
-
+        
         # 初始化节点信息，最终的self.x形状应为(num_assets, node_features)，例如(4, 5+1+1+5=12)
-        # TODO Volume值过大的问题
-        next_obs = torch.from_numpy(self.src.observe().reshape(self.num_asset, -1)) # (4,1,4) -> (4,4)
+        # TODO Volume值过大的问题，暂时抛弃Volume
+        next_obs = torch.from_numpy(price_feature.reshape(self.num_asset, -1))      # (4,1,4) -> (4,4)
         obs = torch.zeros(next_obs.shape)                                           # (4,4)
         action = torch.zeros((self.num_asset,1))                                    # (4,1)
         action[0] = 1                                                               # 初始化动作值默认选择第一个资产100%
@@ -85,26 +75,6 @@ class TemporalPortfolioGraph:
         # PyTorch官方的建议写法：
         self.x = x.clone().detach()
 
-        self.is_reset = True
-
-    # TODO 这个函数没啥必要单独设出来了
-    @reset_check
-    def _add_similarity(self, asset1, asset2, similarity):
-        """
-        Adds a similarity between two assets (asset1 and asset2) into the graph.
-        
-        应当只在初始化图的时候使用
-        
-        Args:
-            asset1: Index of the first asset (node).
-            asset2: Index of the second asset (node).
-            similarity: Similarity value between asset1 and asset2.
-        """
-        # Add edges and weights in both directions for an undirected graph
-        self.weighted_adj[asset1, asset2] = similarity
-        self.weighted_adj[asset2, asset1] = similarity
-
-    @reset_check
     def build_graph(self):
         """
         Builds and returns the graph as a PyG Data object.
@@ -123,7 +93,8 @@ class TemporalPortfolioGraph:
                 # 论文中的初始权重就是各个节点的特征，后续会被替换为GCN的嵌入
                 # 注意Heat_kernel的输入应为np.array
                 similarity = heat_kernel(self.x[i].numpy(), self.x[j].numpy(), lambda_param=2)
-                self._add_similarity(i, j, similarity)
+                self.weighted_adj[i, j] = similarity
+                self.weighted_adj[i, j] = similarity
 
         # Convert adjacency matrix to edge_index and edge_attr
         edge_index = torch.nonzero(self.weighted_adj, as_tuple=False).t().contiguous()
@@ -133,7 +104,6 @@ class TemporalPortfolioGraph:
         data = Data(x=self.x, edge_index=edge_index, edge_attr=edge_attr)
         return data
 
-    @reset_check
     def update_similarity(self, features: np.array):
         """
         Update the node features using the data from pm env.
@@ -146,7 +116,6 @@ class TemporalPortfolioGraph:
         # 转化为张量
         # self.x = torch.tensor(flattened_observation, dtype=torch.float)
 
-    @reset_check
     def observe(self):
         """
         返回的观察值即GCN生成的嵌入向量 (维度为d/2=128)
@@ -157,7 +126,7 @@ class TemporalPortfolioGraph:
 
 
 # Visualization function for NX graph or PyTorch tensor
-def visualize(h, color=None, epoch=None, loss=None, accuracy=None):
+def visualize(h, color="blue", epoch=None, loss=None, accuracy=None):
     plt.figure(figsize=(7,7))
     plt.xticks([])
     plt.yticks([])
@@ -177,7 +146,6 @@ def visualize(h, color=None, epoch=None, loss=None, accuracy=None):
 
 
 if __name__ == "__main__":
-    # Example usage
     target_history, target_stocks = get_history_and_abb()
     data_gen = DataGenerator(target_history, 
                              target_stocks, 
@@ -185,13 +153,16 @@ if __name__ == "__main__":
                              window_length=1,
                              start_idx=0,
                              start_date=None)
-    asset_graph = TemporalPortfolioGraph(data_gen)
-    
     data_gen.reset()
-    asset_graph.reset()
+    
+    num_asset = len(data_gen.asset_names)
+    prices = data_gen.observe()
+    asset_graph = TemporalPortfolioGraph()
+    asset_graph.reset(num_asset, prices)
 
     # Build the PyG Data object
     data = asset_graph.build_graph()
+    print(data)
 
     # 转化为networkx对象以可视化
     G = to_networkx(data, to_undirected=True)
